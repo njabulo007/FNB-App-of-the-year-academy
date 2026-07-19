39a046706d81fcca1d5905f8bbf4e0e218837e4d
Njabulo Dlamini <njabulod007@gmail.com>
2026-07-19 15:24:33 +0200

    contact_book.py works, only a few bugsleft to fix

--- contact_book.py
+++ contact_book.py
@@ -1,7 +1,10 @@
+
+#started with a list with one dictionary inside
 contact_book =[
     {'name' : 'John', 'phone' : '083 345 5559', 'email' : 'john@gmail.com'}
 ]
 
+# a function that adds contacts by appending user input inside the list of dictionary
 def add_contact():
     name = input('Please enter contact name: ') 
     phone = int(input('Please enter contact phone number: ') )
@@ -9,32 +12,37 @@ def add_contact():
     contact_book.append(
         {'name' : name,
         'phone' : phone,
-        'email' : email},
-    print(f"You successfuly added {contact_book['name']}'s contact info")
-
-    )
-
+        'email' : email}
+        )
+    print(f"You successfuly added {contact_book[-1]['name']}'s contact info")
+#a function that takes one argument > name and searches the inputed name and prints it
 def search_name(name):
     for contact_search in contact_book:
         contact_search.get('name')
-        if contact_search not in user_search:
-            print('Not available')
-        else:
+        if contact_search == user_search:
             print(contact_search)
+        elif contact_search != user_search:
+            print('Name not available in contact book, try another name or add it to your contact book by pressing 1')
+        
 
+
+#a function that that takes one argument > name and searches by looping through the list to find the name the user inputed and uses .pop() to remove that user name
 def delete_contact(name):
     
     for contact_delete in contact_book:
         contact_delete.get('name')
-        if contact_delete not in user_delete:
-            print('Not available')
+        if contact_delete != user_delete:
+            print('Name not available in contact book, try another name or add it to your contact book by pressing 1')
         else:
                 contact_book.pop(contact_delete)
 
+#a function that takes no argument and loops through the list and searches for all names and prints them out
 def view_all():
     for contact in contact_book:
-        print(f"{contact['name']}")
+        print(f" name: {contact['name']} --> phone: {contact['phone']} --> email: {contact['email']}")
 
+
+# a while function to make sure code is always running for the user to be able to decide choise. the if and else if statements check user unput and runs the apropriate functions.
 while True:
     user_select = int(input('press 1 to add - 2 to search - 3 to delete - 4 to view all contacts and 5 to exit: '))
     if user_select == 1:
@@ -47,5 +55,7 @@ while True:
         delete_contact(user_delete)
     elif user_select == 4:
         view_all()
-    elif exit == 5:
+    elif user_select== 5:
         break
+    else:
+        print('Enter a valid option')
