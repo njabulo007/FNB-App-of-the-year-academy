b9f23435289e33177ea82e29758ce218c1fc661a
Njabulo Dlamini <njabulod007@gmail.com>
2026-07-20 21:13:51 +0200

    started with grade report.py

---
+++ grade_report.py
@@ -0,0 +1,74 @@
+#• Store at least 5 students as a list of dictionaries: [{name, maths, english, science}, …]
+#• Use a for loop to iterate over all students and calculate each student’s average
+#• Apply the grade/status logic from Unit 5 inside the loop
+#• Build a results list of dictionaries containing: name, average, grade, status
+#• After the main loop, calculate: class average, highest mark, lowest mark
+#• Display a formatted class report showing individual results and class statistics
+#• Use a while loop to let the user search for a student by name after the report is shown
+
+students = [
+    {'name':'Bonolo', 'maths':75, 'english':87, 'science': 69 },
+    {'name':'Sihle', 'maths':72, 'english':90, 'science': 78 },
+    {'name':'Mandla', 'maths':92, 'english':94, 'science': 90 },
+    {'name':'George', 'maths':86, 'english':89, 'science': 78 },
+    {'name':'Amanda', 'maths':96, 'english':88, 'science': 94 }
+]
+
+for student in students:
+    student_average = student['maths'] + student['engish']+ student['science']
+    if student_average >= 80 :
+        learner_grade = 'A'
+    elif student_average >= 70 and student_average <= 79:
+        learner_grade = 'B'
+    elif student_average >= 60 and student_average <= 69:
+        learner_grade = 'C'
+    elif student_average >= 50 and student_average <= 59:
+        learner_grade = 'D'
+    else:
+        learner_grade = 'F' 
+
+    if student_average >= 50:
+        learner_status = "Pass"
+
+    else:
+        learner_status = 'Fail'
+
+
+# a function that produces a class suummary report for all students
+def class_summary_report():
+    return
+
+# a funtion that takes student name and prints their report card.
+def report_card(student_name):
+    return
+
+# a function iterate over all students and calculate each student’s average 
+def average_iteration():
+    return
+
+learner_name = input('Please enter your name: ')
+maths_marks = int(input('Enter your Maths marks: '))
+science_marks = int(input('Enter your Science marks: '))
+english_marks = int(input('Enter your English marks: '))
+
+for student_average in students['maths', 'engish', 'science']:
+    student_average = maths_marks + science_marks + english_marks // 3
+
+
+
+overall_percentage = student_average // 3
+
+print('``' * 20)
+print(f"{learner_name.title()}'s report card")
+print('``' * 20)
+
+print(f' Hello {learner_name.title()}! see your marks below\n Maths: {maths_marks}\n Science: {science_marks}\n English: {english_marks}\n Overall percentage: {overall_percentage}\n Learner status: {learner_status}')
+
+if maths_marks < 40:
+    print(f'You received below {maths_marks} in Maths, subject needs intervention')
+if science_marks < 40:
+    print(f'You received below {science_marks} in Science, subject needs intervention')
+if english_marks < 40:
+    print(f'You received below {english_marks} in English, subject needs intervention')
+
+print('``' * 20)
--- playGround2.py
+++ playGround2.py
@@ -1,9 +1,38 @@
-contact_book =[
-    {'name' : 'John', 'phone' : '083 345 5559', 'email' : 'john@gmail.com'}
+#contact_book =[
+#    {'name' : 'John', 'phone' : '083 345 5559', 'email' : 'john@gmail.com'}
+#]
+#user_search = input('Enter a name to search: ')
+#for contact_search in contact_book:
+#    if contact_search['name'] == user_search:
+#        print(f'Name: {contact_search["name"]}\nPhone: {contact_search["phone"]}\nEmail address: {contact_search["email"]}')
+#    elif contact_search != user_search:
+#        print('Name not available in contact book, try another name or add it to your contact book by pressing 1')
+
+students = [
+    {'name':'Bonolo', 'maths':75, 'english':87, 'science': 69 },
+    {'name':'Sihle', 'maths':32, 'english':20, 'science': 24 },
+    {'name':'Mandla', 'maths':92, 'english':94, 'science': 90 },
+    {'name':'George', 'maths':86, 'english':89, 'science': 78 },
+    {'name':'Amanda', 'maths':96, 'english':88, 'science': 94 }
 ]
-user_search = input('Enter a name to search: ')
-for contact_search in contact_book:
-    if contact_search['name'] == user_search:
-        print(f'Name: {contact_search["name"]}\nPhone: {contact_search["phone"]}\nEmail address: {contact_search["email"]}')
-    elif contact_search != user_search:
-        print('Name not available in contact book, try another name or add it to your contact book by pressing 1')
+
+for student in students:
+    student_average = student['maths'] + student['english']+ student['science']
+    if student_average >= 80 :
+        learner_grade = 'A'
+    elif student_average >= 70 and student_average <= 79:
+        learner_grade = 'B'
+    elif student_average >= 60 and student_average <= 69:
+        learner_grade = 'C'
+    elif student_average >= 50 and student_average <= 59:
+        learner_grade = 'D'
+    else:
+        learner_grade = 'F' 
+
+    if student_average >= 50:
+        learner_status = "Pass"
+
+    else:
+        learner_status = 'Fail'
+
+    print(student_average, learner_grade, learner_status)
